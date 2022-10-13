cff8a961fb1cad88000d842f26324d6328188510
AP Java Period 6 <apjavaperiod6@ericas-iphone.78b505.nycboe.org>
2022-10-13 12:41:32 -0400

    added 06

---
+++ 06-username/UserName.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class UserName {
+  public static void main(String[] args) {
+    // create Scanner object
+    Scanner s = new Scanner(System.in);
+    
+    // get String input from user
+    System.out.print("Are you a student or a teacher?");
+    String choice = s.nextLine();
+    System.out.print("Enter your first name: ");
+    String firstName = s.nextLine();
+    System.out.print("Enter your last name: ");
+    String lastName = s.nextLine();
+    System.out.print("Enter your favorite number: ");
+    String favnumber = s.nextLine();
+    
+    // test output
+    if(choice.equals("Student")) {
+        System.out.print(firstName+initialize(lastName)+favnumber+"@nycstudents.net");
+    }
+    else if (choice.equals("Teacher")) {
+        System.out.print(initialize(firstName)+lastName+favnumber+"@schools.nyc.gov");
+    }
+    else {
+        System.out.print("Please enter 'Student' or 'Teacher'");
+    }
+    
+    s.close();
+  } // end main method
+
+  /*
+   * Name: initialize
+   * Purpose: send back the first character (inital) of a name
+   * Input: a name (String)
+   * Return: a single character (String)
+   */
+  public static String initialize(String n) {
+    return n.substring(0, 1);
+  } // end initialize method
+
+} // end class
